b0cf0bfc2c4b4ca1b62023400196489651229f98
Victor Hugo <hey.zooi@gmail.com>
2020-03-29 12:24:41 -0700

    Updates

---
+++ precache-manifest.b95a7655d64053b09d47ddf5032ff54d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "45451ba573011b5cbc31",
+    "url": "/static/js/app.def0118e.chunk.js"
+  },
+  {
+    "revision": "818911f54acad08a6ef4",
+    "url": "/static/js/runtime~app.86c3a96a.js"
+  },
+  {
+    "revision": "62da637efa49f0b526d9",
+    "url": "/static/js/2.9019fb92.chunk.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "e10ce0552ee7802193950fa9181bd961",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "b333dcaccf681a69a1b0a5058aa06fcb",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "62273473911e68575ace8d48f6ebd399",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "c38e457a0680c791739e51540fb6e99e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9a09d6f438819d4a7e3b19f96474846",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "04cf1ae10e30155ca207400239ec1cbd",
+    "url": "/register-service-worker.js"
+  }
+];
